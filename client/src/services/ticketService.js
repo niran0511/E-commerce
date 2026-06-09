b6e22160511e29ef49d5ffb76e522f1b58b5fb2b
niran0511 <niranjanselvakumar0511@gmail.com>
2026-06-09 20:26:44 +0530

    fixed error

--- client/src/services/ticketService.js
+++ client/src/services/ticketService.js
@@ -1,4 +1,17 @@
-import api from './api';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: { 'Content-Type': 'application/json' }
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('shopsmart-token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 const ticketService = {
   // User creates a ticket
